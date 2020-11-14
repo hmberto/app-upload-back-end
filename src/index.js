8898ad6e12fe618a0341ac28396023fbbef7af61
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-14 03:02:44 -0300

    database connection added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
+
+const userDB = process.env.USER_DB
+const passDB = process.env.PASS_DB
+const clusterDB = process.env.CLUSTER_DB
+const dbName = process.env.DB_NAME
+
+const MONGOOSE_URL = "mongodb+srv://" + userDB + ":" + passDB + "@" + clusterDB + ".m1ekv.mongodb.net/" + dbName + "?retryWrites=true&w=majority"
+
+mongoose.connect(MONGOOSE_URL, {
+  useNewUrlParser: true
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
