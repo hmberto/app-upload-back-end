478cfc90729fa649c47657b9d585377267bfb66c
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-14 04:08:40 -0300

    get and delete routes created

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,31 @@ const multerConfig = require('./config/multer')
 
 const Post = require('./models/Post')
 
+routes.get('/posts', async (req, res) => {
+  const post = await Post.find()
+
+  return res.json(post)
+})
+
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
-  const { originalname: name, size, filename: key } = req.file
+  const { originalname: name, size, filename: key, url = "" } = req.file
 
   const post = await Post.create({
     name,
     size,
     key,
-    url: '',
+    url
   })
 
   return res.json(post)
+})
+
+routes.delete('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id)
+
+  await post.remove()
+
+  return res.send()
 })
 
 module.exports = routes
